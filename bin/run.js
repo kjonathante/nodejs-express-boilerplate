335d49f2aa43905204e977c0fabdfcd00132aba3
Kit Jonathan Te <kjonathante@gmail.com>
2019-08-25 22:55:49 -0700

    Installed express. Created server/index.js and test.

--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,7 @@
 const config = require('../config')
-
+const app = require('../server')(config)
 const log = config.log()
-log.fatal('hello')
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  log.info(`Listening on port ${PORT}`)
+})
--- test/config/index.test.js
+++ test/config/index.test.js
@@ -2,6 +2,6 @@ const config = require('../../config')
 
 describe('Test Configuration Infrastructure', () => {
   test('It should return the correct key', () => {
-    expect(config.secretKey).toBe(process.env.SECRET_KEY)
+    expect(config.privateKey).toBe(process.env.PRIVATE_KEY)
   })
 })
